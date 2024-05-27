92805f55a89abae1022d6282eba1366bdf8bf231
Zamarr0n <emilio.zamarrons@gmail.com>
2024-05-26 20:52:46 -0600

    Implementing the styling to the website, meta tags, html-attributes, etc.

--- getNews.js
+++ getNews.js
@@ -18,37 +18,64 @@ export function getNews(){
             return response.json();
         })
         .then(data => {
+            news_content.innerHTML = "";
             data.news.forEach(element => {
-                console.log('------------------------------------')
                 var section = document.createElement("section");
                 var div = document.createElement("div");
+                var img_div = document.createElement("div");
+                var text_div = document.createElement("div");
+                var btn_div = document.createElement("div");
+                var btn_div_nd = document.createElement("div");
                 var h1 = document.createElement("h1");
                 var paragraph = document.createElement("p");
                 var image = document.createElement("img")
                 var publish_date = document.createElement('p')
                 var author = document.createElement('p');
-
+                var link_to_new = document.createElement('button');
                 h1.textContent = element.title;
                 let short_paragraph = truncateParagraph(element.text, 250)
                 paragraph.textContent = short_paragraph; 
+                paragraph.classList.add('paragraph');
+                author.classList.add('paragraph');
+                publish_date.classList.add('paragraph');
                 image.setAttribute("src",element.image);
                 image.setAttribute("alt","Article Image");
+                image.setAttribute("width","500");
+                image.setAttribute("height","400");
+                image.setAttribute("loading","lazy");
                 publish_date.textContent = element.publish_date;
-                author.textContent = element.author;
-                div.appendChild(image);
+                author.textContent = `Author: ${element.author}`;
+                div.classList.add('news_div');
+                h1.classList.add('title_news');
+                link_to_new.textContent = 'see more  🔎'
+                link_to_new.classList.add("link")
+                link_to_new.classList.add("btn-animation")
+                img_div.appendChild(image);
+                div.appendChild(img_div);
+                img_div.classList.add('img-news')
                 div.appendChild(h1);
-                div.appendChild(paragraph);
-                div.appendChild(publish_date);
+                text_div.appendChild(paragraph)
+                btn_div.appendChild(link_to_new)
+                btn_div_nd.appendChild(btn_div);
+                btn_div.classList.add('link');
+                div.appendChild(text_div);
+                text_div.classList.add('text_center');
+                div.appendChild(link_to_new);
                 div.appendChild(author);
+                div.appendChild(publish_date);
                 section.appendChild(div)
+                section.classList.add('section_news')
                 news_content.appendChild(section);
-                console.log(`title: ${element.title}`)
-                console.log(`text: ${element.text}`)
-                console.log(`url: ${element.url}`)
-                console.log(`image: ${element.image}`)
-                console.log(`publish-date: ${element.publish_date}`)
-                console.log(`author: ${element.author}`)
-                console.log(" ")
+                // console.log(`title: ${element.title}`)
+                // console.log(`text: ${element.text}`)
+                // console.log(`url: ${element.url}`)
+                // console.log(`image: ${element.image}`)
+                // console.log(`publish-date: ${element.publish_date}`)
+                // console.log(`author: ${element.author}`)
+                // console.log(" ")
+                link_to_new.addEventListener("click", function(){
+                    window.location.href = element.url;
+                })
             });
         })
         // .then()
--- search.js
+++ search.js
@@ -2,6 +2,7 @@ import { getNews } from "./getNews.js";
 export function search(){
     let bt_searcher = document.getElementById('search');
     let news_topic = document.getElementById('news_topic').value;
+    let news_div = document.getElementById('news_div');
     if(news_topic === " "){
         console.log('the input is blank')
     }
